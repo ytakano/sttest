4b1d60ae9a0bbfc3ea4e0c06a58e79a375105ae0
Yuuki Takano <ytakanoster@gmail.com>
2021-02-21 22:03:18 +0900

    added an example of offer! macro

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate session_types;
 use session_types as S;
 use std::collections::HashMap;
 use std::thread;
@@ -97,6 +99,55 @@ fn db_client(c: S::Chan<(), DBClient>) {
     let _ = c.zero().sel2().sel2().close();
 }
 
+fn db_server2(c: S::Chan<(), DBServer>) {
+    let mut c_enter = c.enter();
+    let mut db = HashMap::new();
+
+    loop {
+        let c = c_enter;
+        offer! {c,
+            Put => {
+                let (c, key) = c.recv();
+                let (c, val) = c.recv();
+                db.insert(key, val);
+                c_enter = c.zero();
+            },
+            Get => {
+                let (c, key) = c.recv();
+                    let c = if let Some(val) = db.get(&key) {
+                        c.send(Some(*val))
+                    } else {
+                        c.send(None)
+                    };
+                    c_enter = c.zero();
+            },
+            Quit => {
+                c.close();
+                return;
+            }
+        }
+    }
+}
+
+type SChan = S::Chan<(), S::Send<(), S::Eps>>;
+type ChanRecv = S::Recv<SChan, S::Eps>;
+type ChanSend = <ChanRecv as S::HasDual>::Dual;
+
+fn chan_recv(c: S::Chan<(), ChanRecv>) {
+    let (c, cr) = c.recv();
+    c.close();
+    let cr = cr.send(());
+    cr.close();
+}
+
+fn chan_send(c: S::Chan<(), ChanSend>) {
+    let (c1, c2) = S::session_channel();
+    let c = c.send(c1);
+    c.close();
+    let (c2, _) = c2.recv();
+    c2.close();
+}
+
 fn main() {
     let (server_chan, client_chan) = S::session_channel();
     let srv_t = thread::spawn(move || server(server_chan));
@@ -113,6 +164,18 @@ fn main() {
     let (server_chan, client_chan) = S::session_channel();
     let srv_t = thread::spawn(move || db_server(server_chan));
     let cli_t = thread::spawn(move || db_client(client_chan));
+    srv_t.join().unwrap();
+    cli_t.join().unwrap();
+
+    let (server_chan, client_chan) = S::session_channel();
+    let srv_t = thread::spawn(move || db_server2(server_chan));
+    let cli_t = thread::spawn(move || db_client(client_chan));
+    srv_t.join().unwrap();
+    cli_t.join().unwrap();
+
+    let (server_chan, client_chan) = S::session_channel();
+    let srv_t = thread::spawn(move || chan_recv(server_chan));
+    let cli_t = thread::spawn(move || chan_send(client_chan));
     srv_t.join().unwrap();
     cli_t.join().unwrap();
 }
